9e54e66e22cbbfe0c04f5fc576ae9753837ea8d9
Donald <60657226+DonaldKien@users.noreply.github.com>
2020-09-11 16:50:16 +0800

    Updates

---
+++ precache-manifest.43ba0a95e5526c2fb4adf08b72b15482.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d92dc9540c79aa57ab59db4a3ca874b",
+    "url": "/ReactJS_Dashboard/index.html"
+  },
+  {
+    "revision": "7857bedfca40e04b40ef",
+    "url": "/ReactJS_Dashboard/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "f10c072d92d97b925828",
+    "url": "/ReactJS_Dashboard/static/js/2.643919be.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/ReactJS_Dashboard/static/js/2.643919be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7857bedfca40e04b40ef",
+    "url": "/ReactJS_Dashboard/static/js/main.db120335.chunk.js"
+  },
+  {
+    "revision": "c793d95189515697f44b",
+    "url": "/ReactJS_Dashboard/static/js/runtime-main.f996b941.js"
+  }
+]);
